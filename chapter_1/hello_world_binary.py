11614de16463b6514e7b02baccc5db401ba85c38
yanisapths <yanisa.p@arise.com>
2024-08-02 18:00:42 +0700

    first commit

---
+++ chapter_1/hello_world_binary.py
@@ -0,0 +1,16 @@
+def string_to_binary(input_string):
+    return ' '.join(format(ord(char), '08b') for char in input_string)
+
+hello_world = "Hello, World!"
+binary_representation = string_to_binary(hello_world)
+
+
+print(f"Hello, World! in binary is: {binary_representation}")
+
+
+def string_to_ascii(input_string):
+    return ' '.join(str(ord(char)) for char in input_string)
+
+ascii_representation = string_to_ascii(hello_world)
+
+print(f"Hello, World! in ASCII is: {ascii_representation}")
