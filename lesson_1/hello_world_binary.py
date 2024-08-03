3713c0f792097484d338f3c42216d340aefd27ea
yanisapths <yanisa.p@arise.com>
2024-08-03 23:10:17 +0700

    lesson1: html

--- lesson_1/hello_world_binary.py
+++ lesson_1/hello_world_binary.py
@@ -8,9 +8,9 @@ binary_representation = string_to_binary(hello_world)
 print(f"Hello, World! in binary is: {binary_representation}")
 
 
-def string_to_ascii(input_string):
-    return ' '.join(str(ord(char)) for char in input_string)
+# def string_to_ascii(input_string):
+#     return ' '.join(str(ord(char)) for char in input_string)
 
-ascii_representation = string_to_ascii(hello_world)
+# ascii_representation = string_to_ascii(hello_world)
 
-print(f"Hello, World! in ASCII is: {ascii_representation}")
+# print(f"Hello, World! in ASCII is: {ascii_representation}")
